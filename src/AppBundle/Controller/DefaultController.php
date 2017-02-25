d82bd4ac4e5b6ee4fb4258007fb47cca11809748
waski35 <arma@48h.waw.pl>
2017-02-25 01:33:26 +0100

    initial 2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        //return $this->render('default/index.html.twig', array(
-        //    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        //));
+        
+        return $this->render('security/login.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
     }
 }
