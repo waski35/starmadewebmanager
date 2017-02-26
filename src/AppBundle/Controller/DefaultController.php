037943ff0a59d233b85f2d8bd66997563f37f24d
waski35 <arma@48h.waw.pl>
2017-02-26 03:01:56 +0100

    possible fix

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         
-        return $this->render('security/login.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        return $this->redirectToRoute('login');
+
     }
 }
