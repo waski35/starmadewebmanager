648acaf9f9e577c89325d93a51ab08906ed97846
waski35 <arma@48h.waw.pl>
2017-02-26 17:43:55 +0100

    fixes

--- src/AppBundle/Controller/ServerSettingsController.php
+++ src/AppBundle/Controller/ServerSettingsController.php
@@ -12,16 +12,16 @@ class ServerSettingsController extends Controller
     public function showAction(Request $request)
     {
         $shadow_path = $this->container->getParameter('path_to_shadow');
-        $instance_name = $this->container->getParameter('SM_SRV_instance_name');
-        $instance_port = $this->container->getParameter('SM_SRV_instance_port');
-        $instance_host = $this->container->getParameter('SM_SRV_instance_host');
+        //$instance_name = $this->container->getParameter('SM_SRV_instance_name');
+        //$instance_port = $this->container->getParameter('SM_SRV_instance_port');
+        //$instance_host = $this->container->getParameter('SM_SRV_instance_host');
         
 
         return $this->render('serversettings/show.html.twig', array(
             'shadow' => $shadow_path,
-            'instance_name' => $instance_name,
-            'instance_port' => $instance_port,
-            'instance_host' => $instance_host,
+            'instance_name' => "",
+            'instance_port' => "",
+            'instance_host' => "",
         ));
     }
 }
