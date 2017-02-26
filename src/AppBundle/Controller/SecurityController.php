4f59d988aeb87dc2a72cc0ebe8aa752dcaf53497
waski35 <arma@48h.waw.pl>
2017-02-26 03:08:21 +0100

    fix for not imported namespace

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
